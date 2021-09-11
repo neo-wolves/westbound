fc49c2f9fa63305bef78083f08b360126e1ec3b0
neo-wolves <88725399+neo-wolves@users.noreply.github.com>
2021-09-11 02:15:36 +0100

    Update raw.lua

--- raw.lua
+++ raw.lua
@@ -1,3 +1,4 @@
+-- Variables
 local Players = game:GetService('Players')
 local UserInputService = game:GetService('UserInputService')
 local Player = Players.LocalPlayer
@@ -8,69 +9,42 @@ local AutoHealEnabled = false
 local AddonsLaunched = false
 local CurrentShop = ''
 
-local UIClone1 = Player.PlayerGui.MenuButtons:Clone()
-Player.PlayerGui.MenuButtons:Destroy()
-UIClone1.ResetOnSpawn = false
-UIClone1.Parent = Player.PlayerGui
+-- Create UI Elements
+local UILibrary = require(script.Parent.UILibrary)
 
-local UIClone2 = Player.PlayerGui.MenuGui:Clone()
-Player.PlayerGui.MenuGui:Destroy()
-UIClone2.ResetOnSpawn = false
-UIClone2.Parent = Player.PlayerGui
+local Menu = UILibrary.AddMenu()
+local GunMods = UILibrary.AddButton(Menu[1], 'Gun Mods', 'Disabled')
+local AutoBuySell = UILibrary.AddButton(Menu[1], 'Auto Buy/ Sell', 'Disabled')
+local AutoHeal = UILibrary.AddButton(Menu[1], 'Auto Heal', 'Disabled')
+local ESPAdmin = UILibrary.AddButton(Menu[1], 'Launch ESP/ Admin', 'Launch')
 
-local MenuButton = Player.PlayerGui.MenuButtons.Buttons.CallHorse:Clone()
-MenuButton.Name = 'ModMenu'
-MenuButton.ImageColor3 = Color3.fromRGB(117, 117, 117)
-MenuButton.Title.Text = 'Mod Menu'
-MenuButton.Parent = Player.PlayerGui.MenuButtons.Buttons
-
-local MenuFrame = Player.PlayerGui.MenuGui.Menu:Clone()
-MenuFrame.Name = 'ModMenu'
-MenuFrame.MenuTip:Destroy()
-MenuFrame.Title.TextLabel.Text = 'Mod Menu'
-MenuFrame.ScrollingFrame.Settings:Destroy()
-MenuFrame.ScrollingFrame.Map:Destroy()
-MenuFrame.ScrollingFrame.BecomeOutlaw:Destroy()
-MenuFrame.ScrollingFrame.Tutorial.Parent = script
-script.Tutorial.Name = 'Button'
-MenuFrame.Parent = Player.PlayerGui.MenuGui
-
-local GodModeDisplay = Player.PlayerGui.InfoGui.Team:Clone()
-GodModeDisplay.Name = 'GodMode'
-GodModeDisplay.Position = UDim2.fromScale(0.942, 0.88)
-GodModeDisplay.Parent = Player.PlayerGui.MenuGui
-GodModeDisplay.State.Text = 'Not Godded'
-GodModeDisplay.State.Size = UDim2.fromScale(0.847, 1)
-GodModeDisplay.State.Position = UDim2.fromScale(0.074, 0)
-
-game.Players.LocalPlayer.Character.Head.NameTag:Destroy()
-
+-- Global Functions
 local function StartLoop()
 	while wait(0.15) do
 		if AutoBuyEnabled then
 
-		    for _,Shop in pairs(workspace.Shops:GetChildren()) do
-			local Magnitude = (Shop.HumanoidRootPart.Position - Player.Character.HumanoidRootPart.Position).Magnitude
+			for _,Shop in pairs(workspace.Shops:GetChildren()) do
+				local Magnitude = (Shop.HumanoidRootPart.Position - Player.Character.HumanoidRootPart.Position).Magnitude
 
-			if CurrentShop ~= Shop.Name and Magnitude <= 15 then
-				CurrentShop = Shop.Name
+				if CurrentShop ~= Shop.Name and Magnitude <= 15 then
+					CurrentShop = Shop.Name
 
-				game:GetService("ReplicatedStorage").GeneralEvents.BuyItem:InvokeServer("PistolAmmo",true)
-				game:GetService("ReplicatedStorage").GeneralEvents.BuyItem:InvokeServer("RifleAmmo",true)
-				game:GetService("ReplicatedStorage").GeneralEvents.BuyItem:InvokeServer("ShotgunAmmo",true)
-				game:GetService("ReplicatedStorage").GeneralEvents.BuyItem:InvokeServer("Dynamite",true)
-				game:GetService("ReplicatedStorage").GeneralEvents.BuyItem:InvokeServer("SniperAmmo",true)
-				game:GetService("ReplicatedStorage").GeneralEvents.BuyItem:InvokeServer("BIG Dynamite",true)
-				game:GetService("ReplicatedStorage").GeneralEvents.BuyItem:InvokeServer("Health Potion",true)
-				game:GetService("ReplicatedStorage").GeneralEvents.Inventory:InvokeServer("Sell")
-			elseif CurrentShop == Shop.Name and Magnitude > 15 then
-			    CurrentShop = ''
+					game:GetService("ReplicatedStorage").GeneralEvents.BuyItem:InvokeServer("PistolAmmo",true)
+					game:GetService("ReplicatedStorage").GeneralEvents.BuyItem:InvokeServer("RifleAmmo",true)
+					game:GetService("ReplicatedStorage").GeneralEvents.BuyItem:InvokeServer("ShotgunAmmo",true)
+					game:GetService("ReplicatedStorage").GeneralEvents.BuyItem:InvokeServer("Dynamite",true)
+					game:GetService("ReplicatedStorage").GeneralEvents.BuyItem:InvokeServer("SniperAmmo",true)
+					game:GetService("ReplicatedStorage").GeneralEvents.BuyItem:InvokeServer("BIG Dynamite",true)
+					game:GetService("ReplicatedStorage").GeneralEvents.BuyItem:InvokeServer("Health Potion",true)
+					game:GetService("ReplicatedStorage").GeneralEvents.Inventory:InvokeServer("Sell")
+				elseif CurrentShop == Shop.Name and Magnitude > 15 then
+					CurrentShop = ''
+				end
 			end
-		    end
 		end
 
 		if AutoHealEnabled then
-		    local Character = workspace:FindFirstChild(Player.Name)
+			local Character = workspace:FindFirstChild(Player.Name)
 
 			if Character then
 				if Character.Humanoid.Health <= 35 and Character.Humanoid.Health > 0 then
@@ -80,28 +54,6 @@ local function StartLoop()
 		end
 	end
 end
-
-
-Player.CharacterAdded:Connect(function()
-	wait(0.5)
-	
-	game:GetService("ReplicatedStorage").GeneralEvents.CustomizeCharacter:InvokeServer("Shopping", false)
-	StartLoop()
-		
-	GodModeEnabled = false
-	GodModeDisplay.State.Text = 'Not Godded'
-
-	for _,Tool in pairs(Player.Backpack:GetChildren()) do
-		if Tool:IsA('Tool') then
-			Tool.Equipped:Connect(function()
-				GodModeEnabled = false
-				GodModeDisplay.State.Text = 'Not Godded'
-			end)
-		end
-	end
-		
-	game.Players.LocalPlayer.Character.Head.NameTag:Destroy()
-end)
 
 local function SetMods(Mods)
 	for _, Gun in pairs(require(game:GetService("ReplicatedStorage").GunScripts.GunStats)) do
@@ -113,54 +65,57 @@ local function SetMods(Mods)
 	end
 end
 
-local function CreateButton(ButtonName, FunctionCall)
-	local ButtonClone = script.Button:Clone()
-	ButtonClone.Parent = MenuFrame.ScrollingFrame
-	ButtonClone.Name = ButtonName
-	ButtonClone.Title.Text = ButtonName
-	ButtonClone.MouseButton1Click:Connect(FunctionCall)
-end
+Player.CharacterAdded:Connect(function()
+	wait(0.5)
+
+	game:GetService("ReplicatedStorage").GeneralEvents.CustomizeCharacter:InvokeServer("Shopping", false)
+	StartLoop()
+
+	GodModeEnabled = false
+	Menu[2].Text = 'Ungodded'
+
+	for _,Tool in pairs(Player.Backpack:GetChildren()) do
+		if Tool:IsA('Tool') then
+			Tool.Equipped:Connect(function()
+				GodModeEnabled = false
+				Menu[2].Text = 'Ungodded'
+			end)
+		end
+	end
+
+	game.Players.LocalPlayer.Character.Head.NameTag:Destroy()
+end)
 
 UserInputService.InputBegan:Connect(function(Input, GameProcessed)
 	if not GameProcessed then
 		if Input.KeyCode == Enum.KeyCode.Z then
 			if GodModeEnabled then
 				GodModeEnabled = false
-				GodModeDisplay.State.Text = 'Not Godded'
+				Menu[2].Text = 'Ungodded'
 				game:GetService("ReplicatedStorage").GeneralEvents.CustomizeCharacter:InvokeServer("Shopping", false)
 			else
 				game:GetService("ReplicatedStorage").GeneralEvents.CustomizeCharacter:InvokeServer("Shopping", true)
 
 				if game.Players.LocalPlayer.Character:FindFirstChild('ForceField') then
 					GodModeEnabled = true
-					GodModeDisplay.State.Text = 'Godded'
+					Menu[2].Text = 'Godded'
 
 					game.Players.LocalPlayer.Character.ForceField.Visible = false
 				else
-					GodModeDisplay.State.Text = 'No Shop Nearby'
+					Menu[2].Text = 'No Shop Nearby'
 
 					wait(1.5)
 
-					GodModeDisplay.State.Text = 'Not Godded'
+					Menu[2].Text = 'Ungodded'
 				end
 			end
 		end
 	end
 end)
 
-MenuButton.MouseButton1Click:Connect(function()
-	if MenuFrame.Visible then
-		MenuFrame.Visible = false
-	else
-		MenuFrame.Visible = true
-	end
-end)
-
-MenuFrame.Close.MouseButton1Click:Connect(function()
-	MenuFrame.Visible = false
-end)
-
-CreateButton('Gun Mods: Disabled', function()
+-- Element Functions
+-- Gun Mods
+GunMods.MouseButton1Click:Connect(function()
 	local Mods = {
 		FanFire = true, 
 		camShakeResist = 0, 
@@ -173,40 +128,41 @@ CreateButton('Gun Mods: Disabled', function()
 		ZoomAccuracy = 0, 
 		InstantFireAnimation = true
 	}
+	
 	SetMods(Mods)
-
-	Player.PlayerGui.MenuGui.ModMenu.ScrollingFrame:FindFirstChild('Gun Mods: Disabled').Title.Text = 'Gun Mods: Enabled'
+	
+	GunMods.TextLabel.Text = 'Enabled'
 end)
 
-CreateButton('Auto Buy/ Sell: Disabled', function()
+-- Auto Buy/ Sell
+AutoBuySell.MouseButton1Click:Connect(function()
 	if AutoBuyEnabled then
 		AutoBuyEnabled = false
-		Player.PlayerGui.MenuGui.ModMenu.ScrollingFrame:FindFirstChild('Auto Buy/ Sell: Disabled').Title.Text = 'Auto Buy/ Sell: Disabled'
+		AutoBuySell.TextLabel.Text = 'Disabled'
 	else
 		AutoBuyEnabled = true
-		Player.PlayerGui.MenuGui.ModMenu.ScrollingFrame:FindFirstChild('Auto Buy/ Sell: Disabled').Title.Text = 'Auto Buy/ Sell: Enabled'
+		AutoBuySell.TextLabel.Text = 'Enabled'
 	end
 end)
 
-CreateButton('Auto Heal: Disabled', function()
-	if AutoHealEnabled then
+-- Auto Heal
+AutoHeal.MouseButton1Click:Connect(function()
+	if AutoBuyEnabled then
 		AutoHealEnabled = false
-		Player.PlayerGui.MenuGui.ModMenu.ScrollingFrame:FindFirstChild('Auto Heal: Disabled').Title.Text = 'Auto Heal: Disabled'
+		AutoHeal.TextLabel.Text = 'Disabled'
 	else
 		AutoHealEnabled = true
-		Player.PlayerGui.MenuGui.ModMenu.ScrollingFrame:FindFirstChild('Auto Heal: Disabled').Title.Text = 'Auto Heal: Enabled'
+		AutoHeal.TextLabel.Text = 'Enabled'
 	end
 end)
 
-CreateButton('Enable ESP & FE Admin', function()
+-- ESP/ Admin
+ESPAdmin.MouseButton1Click:Connect(function()
 	if not AddonsLaunched then
 		AddonsLaunched = true
 		loadstring(game:HttpGet('https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua'))()
 		loadstring(game:HttpGet('http://impulse-hub.xyz/ImpulseIY',true))()
+		
+		ESPAdmin.TextLabel.Text = 'Launched'
 	end
 end)
-
-CreateButton('Remote Spy (Debug Only)', function()
-	loadstring(game:HttpGet("https://pastebin.com/raw/BDhSQqUU", true))()
-end) 
-
