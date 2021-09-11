fbbb2a46416afbf079daa779b7152a2ccb32e001
neo-wolves <88725399+neo-wolves@users.noreply.github.com>
2021-09-11 02:39:24 +0100

    Update raw.lua

--- raw.lua
+++ raw.lua
@@ -19,7 +19,7 @@ UILibrary.AddButton(Menu[1], 'Launch ESP/ Admin', 'Launch')
 
 local Buttons = {}
 
-for _,Frame in pairs(Menu[2]:GetChildren()) do
+for _,Frame in pairs(Menu[1]:GetChildren()) do
 	print('Test', Frame)
 	if Frame:IsA('Frame') then
 		Buttons[Frame.TextLabel.Text] = Frame.ImageButton
